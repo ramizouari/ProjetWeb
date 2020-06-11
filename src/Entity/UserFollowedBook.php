689c85e1989f33cf254002e19fd128d83cd06ba0
ramizouari <rami.zouari@insat.u-carthage.tn>
2020-06-11 22:49:34 +0100

    Log in + Sign up

---
+++ src/Entity/UserFollowedBook.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserFollowedBookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserFollowedBookRepository::class)
+ */
+class UserFollowedBook
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $userId;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bookId;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(int $userId): self
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    public function getBookId(): ?int
+    {
+        return $this->bookId;
+    }
+
+    public function setBookId(int $bookId): self
+    {
+        $this->bookId = $bookId;
+
+        return $this;
+    }
+}
