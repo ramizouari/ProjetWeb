d1773ed49d4d0564e3e3951a4b3049fba57a43ff
souheil <souheilbenslama@insat.u-carthage.tn>
2020-06-17 22:51:15 +0100

    mes echanges ...

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -28,33 +28,105 @@ class ProfileController extends AbstractController
 
             $pubs=null ;
     //}
-
-        return$this->render("profile/journale.html.twig",["pubs"=>$pubs,"user"=>$this->getUser()]);
-
-    }
+        return$this->render("profile/journale.html.twig",["pubs"=>$pubs,"user"=>$this->getUser()]);  }
 
 
     /**
      * @Route("/meslivres" , name="meslivres")
      */
-
-    public function meslivres()
+    public function meslivres(Request $request)
     {
-
         $rep=$this->getDoctrine()->getRepository("App:UserBook");
         $livres=$rep->findBy(["userId"=>$this->getUser()->getId()]);
-
         $rep2=$this->getDoctrine()->getRepository("App:Book");
-        $meslivre=array();
-        $i=0;
-      foreach ($livres as $livre){
-
-          $meslivre[$i]=$rep2->findOneBy(["id"=>$livre->getBookId()]);
-            $i++;
-           }
-
-        return $this->render("profile/meslivres.html.twig",array("meslivres"=>$meslivre, "user"=>$this->getUser()));
+        $meslivres=array();
+        $j=0;
+        foreach ($livres as $livre){
+            $meslivres[$j]=$rep2->findOneBy(["id"=>$livre->getBookId()]);
+            $j++;
+        }
+        $numb=count($meslivres);
+        $pagenumb=($numb %12)? ((integer)($numb/12) +1) :($numb/12) ;
+        $page=$request->query->get("page") ?? 1 ;
+        if(($page<=0)){$page=1;}
+        if($page>=$pagenumb) {$page=$pagenumb;}
+        $pagetable=array();
+        for($i=0;$i<12;$i++)
+        {      if(($page==$pagenumb)&&(($i)==($numb%12)))
+                {break;}
+                $pagetable[$i]=$meslivres[($page-1)*12+$i];}
+        return $this->render("profile/meslivres.html.twig",array("meslivres"=>$pagetable, "user"=>$this->getUser(),"nbpages"=>$pagenumb,"page"=>$page));
 
     }
 
+    /**
+     * @Route("/liv_abon" , name="livres_abonnements")
+     */
+
+    public function get_abon(Request $request)
+    {
+        $rep=$this->getDoctrine()->getRepository("App:UserFollowedBook");
+        $livres=$rep->findBy(["userId"=>$this->getUser()->getId()]);
+        $rep2=$this->getDoctrine()->getRepository("App:Book");
+        $meslivres=array();
+        $j=0;
+        foreach ($livres as $livre){
+            $meslivres[$j]=$rep2->findOneBy(["id"=>$livre->getBookId()]);
+            $j++;
+        }
+        $numb=count($meslivres);
+        $pagenumb=($numb %12)? ((integer)($numb/12) +1) :($numb/12) ;
+        $page=$request->query->get("page") ?? 1 ;
+        if(($page<=0)){$page=1;}
+        if($page>=$pagenumb) {$page=$pagenumb;}
+        $pagetable=array();
+        for($i=0;$i<12;$i++)
+        {
+            if(($page==$pagenumb)&&(($i)==($numb%12)))
+            {break;}
+            $pagetable[$i]=$meslivres[($page-1)*12+$i];
+        }
+        return $this->render("profile/meslivres.html.twig",array("meslivres"=>$pagetable,"user"=>$this->getUser(),"nbpages"=>$pagenumb,"page"=>$page));
+    }
+
+
+
+/**
+ * @Route("/echanges" , name="echanges")
+ *
+ */
+
+public function echanges(Request $request)
+{
+    $rep=$this->getDoctrine()->getRepository("App:Exchange");
+    $requesterechanges=$rep->findBy(["requesterId"=>$this->getUser()->getId()]);
+    $mesdemandes=array();
+    $rep1=$this->getDoctrine()->getRepository("App:Book");
+    $rep2=$this->getDoctrine()->getRepository('App:User');
+    $i=0;
+    foreach ($requesterechanges as $echange){
+        $mesdemandes[$i]=["id"=>$echange->getId(),
+                          "requesteduser"=>$rep2->findOneBy(["id" => $echange->getResponderId()])->getUsername(),
+                           "requestedbook"=>$rep1->findOneBy(["id"=>$echange->getRequestedBookId()])->getTitle(),
+                            "availablebook"=>$rep1->findOneBy(["id"=>$echange->getRespondedBookId()])->getTitle()
+                            ];
+        $i++;
+    }
+
+
+    $responderechanges=$rep->findBy(["responderId"=>$this->getUser()->getId()]);
+    $mesreceived=array();
+    foreach ($responderechanges as $echange){
+        $mesreceived[$i]=["id"=>$echange->getId(),
+            "requesteruser"=>$rep2->findOneBy(["id" => $echange->getRequesterId()])->getUsername(),
+            "requestedbook"=>$rep1->findOneBy(["id"=>$echange->getRequestedBookId()])->getTitle(),
+            "availablebook"=>$rep1->findOneBy(["id"=>$echange->getRespondedBookId()])->getTitle()
+        ];
+        $i++;
+    }
+
+    //$demandesrecuesob=$rep->findBy(["responderId"=>$this->getUser()->getId()]);
+    return $this->render("profile/mesechanges.html.twig",array("mesdemandes"=>$mesdemandes,"mesreponses"=>$mesreceived,"user"=>$this->getUser()));
 }
+}
+
--- src/DataFixtures/Exchange.php
+++ src/DataFixtures/Exchange.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class Exchange extends Fixture
 {
@@ -12,6 +13,29 @@ class Exchange extends Fixture
         // $product = new Product();
         // $manager->persist($product);
 
+        $faker=Factory::create();
+        for($i=0;$i<200;$i++)
+        {
+          $echange= new Exchange();
+          $echange->setRequesterId($faker->numberBetween(1,200));
+          $echange->setResponderId($faker->numberBetween(1,200));
+          $echange->setState($faker->numberBetween(0,3));
+          $echange->setRequestedBookId($faker->numberBetween(1,200));
+          $echange->setRespondedBookId($faker->numberBetween(1,200));
+          $manager->persist($echange);
+
+    }
         $manager->flush();
     }
+
+    public function getDependencies()
+    {
+        return array(
+            UserFixtures::class,
+            userFixtures::class,
+            BookFixtures::class,
+            BookFixtures::class
+        );
+    }
+
 }
